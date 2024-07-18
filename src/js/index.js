85f02584b1108406ba0d5320798d613725a9a473
Vasiliy Igonin <vigonin83@gmail.com>
2024-07-18 22:57:39 +0300

    predeploy build

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ import './libs/modal.js'
 import './libs/changeImage.js'
 import './libs/swiper.js'
 import './libs/mobileMenu.js'
+import './libs/filterRooms.js'
 
 console.log('Project has been started...')
